b7e8aee1350aef17c5c6d2ea7f2afe3579e4b1ad
ddddddeon <chris.deon@unity3d.com>
2023-03-04 19:44:02 -0500

    start on repl

--- src/cgpt/repl.rs
+++ src/cgpt/repl.rs
@@ -1,4 +1,5 @@
 use super::client::Client;
+use std::io::{self, Write};
 
 pub struct Repl {
     name: String,
@@ -17,6 +18,21 @@ impl Repl {
         let mut client = Client::new();
         client.connect()?;
         self.running = true;
+
+        let mut input = String::new();
+
+        loop {
+            print!("> ");
+            io::stdout().flush().unwrap();
+            if let Err(e) = io::stdin().read_line(&mut input) {
+                println!("Could not read from stdin: {e}");
+                return Err(e.into());
+            }
+
+            print!("You entered: {input}");
+            // TODO save message history
+            input.clear();
+        }
 
         Ok(())
     }
